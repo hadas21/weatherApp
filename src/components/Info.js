4e762f501e86ed5d054499387e8c6e7dd6eef3a8
Hadas Axelbaum <hadasa@inmedmd.com>
2023-02-26 10:01:59 +0200

    Revert "Merge branch 'practice-revert-2'"
    This reverts commit d84bf54ad23284e454567adf7b9c62c0df81cc54, reversing
    changes made to 304376c238eb9f7710ac44646972b9ab4f3770e7.

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,31 +1,33 @@
 // import React
-import React, { useState } from "react";
-import Location from "./Location";
-import Weather from "./Weather";
+import React, {useState} from 'react'
+import Location from './Location'
+import Weather from './Weather'
 
 const info = {
-  background: "pink",
-  display: "flex",
-  justifyContent: "top",
-  flexDirection: "column",
-  color: "#d2524f",
-  padding: "6rem 2rem",
-};
+	background: '#2b3746',
+	display: 'flex',
+	justifyContent: 'top',
+	flexDirection: 'column',
+	color: '#d2524f',
+	padding: '6rem 2rem'
+}
 
 const Info = (props) => {
-  const [location, setLocation] = useState({ city: "", country: "" });
-  const [data, setData] = useState("");
 
-  return (
-    <section style={info}>
-      <Location
-        location={location}
-        setLocation={setLocation}
-        setData={setData}
-      ></Location>
-      <Weather data={data}></Weather>
-    </section>
-  );
-};
+	const [location, setLocation] = useState({ city: '', country: '' })
+	const [data, setData] = useState('')
 
-export default Info;
+
+
+	return (
+		<section style={info}>
+			<Location
+				location={location}
+				setLocation={setLocation}
+				setData={setData}></Location>
+			<Weather data={data}></Weather>
+		</section>
+	)
+}
+
+export default Info
