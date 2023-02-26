de0ea908d5fc5cc8f16722dce640668886194298
Hadas Axelbaum <hadasa@inmedmd.com>
2023-02-26 10:01:03 +0200

    change backgroung

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,33 +1,31 @@
 // import React
-import React, {useState} from 'react'
-import Location from './Location'
-import Weather from './Weather'
+import React, { useState } from "react";
+import Location from "./Location";
+import Weather from "./Weather";
 
 const info = {
-	background: '#2b3746',
-	display: 'flex',
-	justifyContent: 'top',
-	flexDirection: 'column',
-	color: '#d2524f',
-	padding: '6rem 2rem'
-}
+  background: "pink",
+  display: "flex",
+  justifyContent: "top",
+  flexDirection: "column",
+  color: "#d2524f",
+  padding: "6rem 2rem",
+};
 
 const Info = (props) => {
+  const [location, setLocation] = useState({ city: "", country: "" });
+  const [data, setData] = useState("");
 
-	const [location, setLocation] = useState({ city: '', country: '' })
-	const [data, setData] = useState('')
+  return (
+    <section style={info}>
+      <Location
+        location={location}
+        setLocation={setLocation}
+        setData={setData}
+      ></Location>
+      <Weather data={data}></Weather>
+    </section>
+  );
+};
 
-
-
-	return (
-		<section style={info}>
-			<Location
-				location={location}
-				setLocation={setLocation}
-				setData={setData}></Location>
-			<Weather data={data}></Weather>
-		</section>
-	)
-}
-
-export default Info
+export default Info;
